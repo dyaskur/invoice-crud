fda7e1cd5e56fa475f582c3ee0e9d8f6826b1cd9
Yaskur 🇲 <dyas@yaskur.com>
2021-12-22 09:47:57 +0700

    add how to install to README.md

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function() {
-    return view('welcome');
+    return redirect(\route('invoices.index'));
 });
 Route::get('/invoices/json', [App\Http\Controllers\InvoiceController::class, 'json'])->name('invoices.json');
 
