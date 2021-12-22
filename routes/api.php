6933f25d67604d01c019f9e395eb4a9c6c42fe6f
Yaskur 🇲 <dyas@yaskur.com>
2021-12-22 08:42:59 +0700

    [new] Invoice API details

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,10 @@ Route::get('/invoices', function(Request $request) {
 });
 
 Route::get('/invoices/{id}', function(Request $request, $id) {
-    $invoice = Invoice::where('invoice_number', $id)->first();
+    $invoice = Invoice::where('invoice_number', $id)
+        ->with('issuer:id,name,address_1,address2', 'recipient:id,name,address_1,address2')
+        ->with('items')
+        ->first();
     if (!$invoice) {
         return response()->json(['data' => null, 'message' => 'Record not found'], 404);
     }
